4e86b1b4a843367016f7d4141a0bb51cf00d3d15
Hojun_Kim <ghwns5338@gmail.com>
2019-12-19 22:50:50 +0900

    퍼미션,위도,경도 정보 가져오기

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Alert } from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
 
 export default class extends React.Component {
-  
   getLocation= async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log({location});
+    try {
+      await Location.requestPermissionsAsync();
+      const {coords : {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      console.log(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Cant't find you.", "So sad");
+    }
   };
   componentDidMount(){
     this.getLocation();
-  }
+  };
   render(){
     return <Loading />;
   } 

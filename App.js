e97973bf0bd710f55ccc8eee0b7f94efd8c8ebf8
Hojun_Kim <ghwns5338@gmail.com>
2019-12-21 15:57:25 +0900

    lat,lon 정보로 openweatherAPI의 데이터 불러오기

--- App.js
+++ App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import { Alert } from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
+import axios from 'axios';
+
+const API_KEY = "007ced0339fb4d90ae6241289212a5b8";
 
 export default class extends React.Component {
   state = {
     isLoading : true
   }
+  getWeather = async(latitude, longitude) => {
+    const {data} = await axios.get(`api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`);
+    console.log(data);
+  } //`${}` called template strings es6
   getLocation= async () => {
     try {
       await Location.requestPermissionsAsync();
       const {coords : {latitude, longitude}} = await Location.getCurrentPositionAsync();
-      this.setState({isLoading : true});
+      this.getWeather(latitude, longitude);
+      this.setState({isLoading : false})
     } catch (error) {
       Alert.alert("Cant't find you.", "So sad");
     }

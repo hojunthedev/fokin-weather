54ca3c285648f9799ed3baeeafe02f35eb3b76af
Hojun_Kim <ghwns5338@gmail.com>
2019-12-22 23:07:12 +0900

    Weather component 추가, 1222현재 EXPO가 NETWORK 권한못읽음

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Alert } from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
 import axios from 'axios';
+import Weather from './Weather';
 
 const API_KEY = "007ced0339fb4d90ae6241289212a5b8";
 
@@ -11,15 +12,14 @@ export default class extends React.Component {
     isLoading : true
   }
   getWeather = async(latitude, longitude) => {
-    const {data} = await axios.get(`api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`);
-    console.log(data);
-  } //`${}` called template strings es6
+    const {data} = await axios.get(`api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`);
+    this.setstate({isLoading : false, temp : data.main.temp})
+  }; //`${}` called template strings es6
   getLocation= async () => {
     try {
       await Location.requestPermissionsAsync();
       const {coords : {latitude, longitude}} = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      this.setState({isLoading : false})
     } catch (error) {
       Alert.alert("Cant't find you.", "So sad");
     }
@@ -28,7 +28,7 @@ export default class extends React.Component {
     this.getLocation();
   };
   render(){
-    const {isLoading} = this.state;
-    return isLoading ?<Loading /> : null;
+    const {isLoading, temp} = this.state;
+    return isLoading ? <Loading /> : <Weather temp = {Math.round(temp)}/>;
   } 
 }

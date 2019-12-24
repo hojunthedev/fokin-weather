3e83319141e7d328eef5d2bf4fbab43bf4d9487f
Hojun_Kim <ghwns5338@gmail.com>
2019-12-24 23:24:45 +0900

    드디어 data 정보 습득 성공

--- App.js
+++ App.js
@@ -5,25 +5,23 @@ import * as Location from 'expo-location';
 import axios from 'axios';
 import Weather from './Weather';
 
-const API_KEY = "4de2b5b882017390c6ffc645594a3227";
+const API_KEY = "007ced0339fb4d90ae6241289212a5b8";
 
 export default class extends React.Component {
   state = {
     isLoading : true
   }
   
-  getWeather = async(lat, lon) => {
-    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`).catch(function (error) {
-      console.log(error.response);
- });
-    this.setstate({isLoading : false, temp : data.main.temp});
-  } //`${}` called template strings es6
+  getWeather = async(latitude, longitude) => {
+    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`);
+    this.setState({isLoading : false, temp : data.main.temp});
+  }
   
   getLocation= async () => {
     try {
       await Location.requestPermissionsAsync();
-      const {coords : {lat, lon}} = await Location.getCurrentPositionAsync();
-      this.getWeather(lat, lon);
+      const {coords : {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
     } catch (error) {
       Alert.alert("Cant't find you.", "So sad");
     }
@@ -35,6 +33,6 @@ export default class extends React.Component {
 
   render(){
     const {isLoading, temp} = this.state;
-    return isLoading ? <Loading /> : <Weather temp = {Math.round(temp)}/>;
+    return isLoading ? ( <Loading /> ) : (<Weather temp = {Math.round(temp)}/>);
   } 
 }

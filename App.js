4f7daaf5f0358163e5e7205df742f56a2bba534d
Hojun_Kim <ghwns5338@gmail.com>
2020-01-07 22:18:24 +0900

    현재 날씨정보 습득

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default class extends React.Component {
   
   getWeather = async(latitude, longitude) => {
     const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`);
-    this.setState({isLoading : false, temp : data.main.temp});
+    this.setState({isLoading : false, condition : "Clear", temp : data.main.temp});
   }
   
   getLocation= async () => {
@@ -32,7 +32,7 @@ export default class extends React.Component {
   }
 
   render(){
-    const {isLoading, temp} = this.state;
-    return isLoading ? ( <Loading /> ) : (<Weather temp = {Math.round(temp)}/>);
+    const {isLoading, temp, condition} = this.state;
+    return isLoading ? ( <Loading /> ) : (<Weather temp = {Math.round(temp)} condition ={condition} />);
   } 
 }
--- Weather.js
+++ Weather.js
@@ -12,7 +12,8 @@ export default function Weather({temp}){
 }
 
 Weather.propTypes = {
-    temp : PropTypes.number.isRequired
+    temp : PropTypes.number.isRequired,
+    condition : PropTypes.oneOf(["Thunderstorm","Drizzle","Rain","Snow", "Atmosphere","Clear","Clouds"]).isRequired
 };
 
 const styles = StyleSheet.create({
